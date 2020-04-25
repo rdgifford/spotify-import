45b34a346a4c6563a8996033114593ad76dccc3c
Glitch (spotify-import) <none>
2020-04-25 22:45:19 +0000

    🌻📖 Updated with Glitch

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,6 +2,6 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 ReactDOM.render(
-  <div>{"hello"}</div>,
+  <div>{"hello now x6?"}</div>,
   document.getElementsByTagName('main')[0]
 )
